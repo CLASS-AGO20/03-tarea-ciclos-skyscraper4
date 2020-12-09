300050e84ab26ed01d84277cae97cef1c40fd1da
skyscraper4 <jcardenas1@ucol.mx>
2020-12-09 00:11:00 -0600

    Implemented sumatoriaSerieDos

--- src/main.js
+++ src/main.js
@@ -7,8 +7,24 @@ export default class App {
         }
         return suma;
     }
+
+    sumatoriaSerieDos(numero) {
+     let z = 2;
+     let suma = 1;
+        while(z <= numero) {
+
+            if(z%2 == 0) {
+                suma = suma + (1/z);
+            } else if(z%2 != 0) {
+                suma = suma - (1/z);
+            }
+            z = z + 1;
+        }
+        return suma;
+    }
 }
 
 let app = new App();
 
 console.log(app.sumatoriaSerieUno(5));
+console.log(app.sumatoriaSerieDos(5));
